33a5bcc18d4261371c233351c62388c4dd4e2fb7
Dirk <dirk@pooliestudios.com>
2018-01-11 11:44:40 +0100

    PlugIn-Informationen angepasst

--- payone-woocommerce-3.php
+++ payone-woocommerce-3.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Plugin Name: Payone Woocommerce 3
+Plugin Name: BS PAYONE WooCommerce 3
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
-Author: dolbertz
+Author: BS PAYONE
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
