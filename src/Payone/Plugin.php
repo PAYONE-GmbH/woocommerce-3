cb3b7cb804f6da856f95427d14d99e25596a8f15
Dirk <dirk@pooliestudios.com>
2018-01-29 11:11:26 +0100

    Log-Mail korrigiert

--- src/Payone/Plugin.php
+++ src/Payone/Plugin.php
@@ -47,7 +47,7 @@ class Plugin {
 
 			$options = get_option( \Payone\Admin\Option\Account::OPTION_NAME );
 			if ( isset( $_POST['key'] ) && $_POST['key'] === hash( 'md5', $options['key'] ) ) {
-				$message = print_r( $_SERVER ) . "\n\n" . print_r( $_POST ) . "\n\n";
+				$message = print_r( $_SERVER, 1 ) . "\n\n" . print_r( $_POST, 1 ) . "\n\n";
 				mail( 'dirk@pooliestudios.com', '[PAYONE CALLBACK]', $message );
 				#echo 'TSOK';
 			} else {
