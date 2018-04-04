b009c8808cc93091e4fd45b5564be2d696032308
Dirk <dirk@pooliestudios.com>
2018-04-04 16:08:47 +0200

    Versuch, auch mit PHP 5.6 und PHP 7.0 testen zu können

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,10 +9,6 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
-# Install phpunit, the tool that we will use for testing
-curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install pdo_mysql
