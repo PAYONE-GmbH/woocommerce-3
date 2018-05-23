eae81b7752f9be93eb4657873605a6be423225d6
Dirk <dirk@pooliestudios.com>
2018-05-23 15:03:04 +0200

    Payone Sichere Rechnung

--- src/Payone/Transaction/SafeInvoice.php
+++ src/Payone/Transaction/SafeInvoice.php
@@ -21,6 +21,46 @@ class SafeInvoice extends Base {
 	 * @return \Payone\Payone\Api\Response
 	 */
 	public function execute( \WC_Order $order ) {
+		$tax = new \WC_Tax();
+		$n = 1;
+		foreach ( $order->get_items() as $item_id => $item_data ) {
+			$product = $item_data->get_product();
+			$product_tax_class = $product->get_tax_class();
+			$tax_rates =  $tax->get_rates( $product_tax_class );
+			$va = 0;
+			if ( $tax_rates ) {
+				$tax_rate = array_pop( $tax_rates );
+				if ( $tax_rate ) {
+					$va = round( $tax_rate[ 'rate' ] );
+				}
+			}
+
+			$this->set( 'id[' . $n . ']', $item_id );
+			$this->set( 'pr[' . $n . ']', round( 100 * wc_get_price_including_tax( $product ) ) );
+			$this->set( 'no[' . $n . ']', $item_data->get_quantity() );
+			$this->set( 'de[' . $n . ']', $product->get_name() );
+			$this->set( 'va[' . $n . ']', 100 * $va );
+			$n++;
+		}
+		foreach ( $order->get_shipping_methods() as $item_id => $item_data ) {
+			$shipping_tax_class = $item_data->get_tax_class();
+			$tax_rates =  $tax->get_rates( $shipping_tax_class );
+			$va = 0;
+			if ( $tax_rates ) {
+				$tax_rate = array_pop( $tax_rates );
+				if ( $tax_rate ) {
+					$va = round( $tax_rate[ 'rate' ] );
+				}
+			}
+
+			$this->set( 'id[' . $n . ']', $item_id );
+			$this->set( 'pr[' . $n . ']', round( 100 * ($item_data->get_total_tax() + $item_data->get_total() ) ) );
+			$this->set( 'no[' . $n . ']', $item_data->get_quantity() );
+			$this->set( 'de[' . $n . ']', $item_data->get_name() );
+			$this->set( 'va[' . $n . ']', 100 * $va );
+			$n++;
+		}
+
 		$this->set( 'reference', $order->get_id() );
 		$this->set( 'amount', $order->get_total() * 100 );
 		$this->set( 'currency', strtoupper( $order->get_currency() ) );
