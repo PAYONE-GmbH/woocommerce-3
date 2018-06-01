42943d5e7bce3725bcaed030e1a57bb288781650
Dirk <dirk@pooliestudios.com>
2018-06-01 16:30:49 +0200

    Testcase für Payones internes Testsystem

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -14,5 +14,6 @@ final class PluginTest extends TestCase {
 		$this->assertFalse( \Payone\Plugin::ip_address_is_in_range( '192.168.0.1', '213.178.72.196' ) );
 		$this->assertFalse( \Payone\Plugin::ip_address_is_in_range( '192.168.0.1', '185.60.20.0/24' ) );
 		$this->assertFalse( \Payone\Plugin::ip_address_is_in_range( '185.60.21.1', '185.60.20.0/24' ) );
+		$this->assertTrue( \Payone\Plugin::ip_address_is_in_range( '192.168.65.178', '192.168.65.178' ) );
 	}
 }
