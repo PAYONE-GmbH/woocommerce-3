c47817244b0fb3ed710eb4c2b01ec4367d7562ba
Dirk Olbertz <dirk@pooliestudios.com>
2019-01-25 12:16:23 +0100

    Changed discount calculation
    WooCommerce does not reliably return the value for $order->get_total_discount(). Therefore we calculate the discount as difference between the total amount of an order an its positions.

--- src/Payone/Transaction/Base.php
+++ src/Payone/Transaction/Base.php
@@ -116,7 +116,7 @@ class Base extends Request {
             $va = Plugin::get_tax_rate_for_item_data( $data );
             $price_all = $data[ 'subtotal' ] + $data[ 'subtotal_tax' ];
             $price_one = $price_all / $item_data->get_quantity();
-            $price = round( 100 * $price_one );
+            $price = (int)round( 100 * $price_one );
 			$articles[ $n ] = [
 				'id' => $item_id,
 				'pr' => $price,
@@ -126,13 +126,13 @@ class Base extends Request {
 			];
 			$n++;
 		}
-
 		foreach ( $order->get_shipping_methods() as $item_id => $item_data ) {
 			$data = $item_data->get_data();
 			$va = Plugin::get_tax_rate_for_item_data( $data );
+			$price = (int)round( 100 * ( $data[ 'total' ] + $data[ 'total_tax' ] ) );
 			$articles[ $n ] = [
 				'id' => $item_id,
-				'pr' => round( 100 * ( $data[ 'total' ] + $data[ 'total_tax' ] ) ),
+				'pr' => $price,
 				'no' => 1,
 				'de' => $data[ 'name' ],
 				'va' => 100 * $va,
@@ -140,19 +140,21 @@ class Base extends Request {
 			$n++;
 		}
 
-        $discount_tax = round( (float)$order->get_discount_tax(), 2);
-        $price = round( 100 * ( (float)$order->get_total_discount() + $discount_tax ) );
-		if ($price > 0) {
+        $discountAmount = round($order->get_total() * 100);
+		foreach ($articles as $article) {
+            $discountAmount = $discountAmount - ($article['pr'] * $article['no']);
+        }
+        if ($discountAmount < 0) {
 			$articles[ $n ] = [
 				'id' => -1,
-				'pr' => - $price,
+				'pr' => $discountAmount,
 				'no' => 1,
 				'de' => __( 'Discount', 'payone-woocommerce-3' ),
 				'va' => 0,
 			];
 			$this->set( 'va[' . $n . ']', 0 );
 		}
-
+        
 		return $articles;
 	}
 }
