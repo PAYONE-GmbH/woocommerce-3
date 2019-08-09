c9581db36ba19d1c25e417edfe813a3c12d15f84
Fabian Boettcher <fabian.boettcher@bspayone.com>
2019-08-09 11:15:51 +0200

    Fix logical error of IP address retrieval

--- src/Payone/Transaction/Base.php
+++ src/Payone/Transaction/Base.php
@@ -109,20 +109,21 @@ class Base extends Request {
     }
 
     /**
-     * Sets PAYONE API customer IP parameter.
+     * Sets PAYONE API customer IP from the provided WooCommerce order.
      *
      * @author Fabian Böttcher <fabian.boettcher@payone.de>
+     * @param \WC_Order $order The WooCommerce order object.
      * @return void
      */
-    protected function set_customer_ip_from_order()
+    protected function set_customer_ip_from_order( \WC_Order $order )
     {
         // Get IP from order object.
-        $ip = \WC_Geolocation::get_ip_address();
+        $ip = get_post_meta($order->get_id(), '_customer_ip_address', true);
 
         // Validate the customer IP.
         $ip = filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
 
-        // Append a valid IP address to PAYINE API request data.
+        // Append a valid IP address to PAYONE API request data.
         if ($ip) {
             $this->set( 'ip', $ip );
         }
