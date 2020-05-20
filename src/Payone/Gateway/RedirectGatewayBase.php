57ae66dc1ec23b5024bcdf14430cd4bc61c60e29
Fabian Böttcher <fabian.boettcher@payone.com>
2020-05-20 10:48:08 +0200

    Fix processing of redirect payments

--- src/Payone/Gateway/RedirectGatewayBase.php
+++ src/Payone/Gateway/RedirectGatewayBase.php
@@ -3,38 +3,56 @@
 namespace Payone\Gateway;
 
 abstract class RedirectGatewayBase extends GatewayBase {
-	/**
+
+    /**
+     * This method is responsible for handling both the payment initiation
+     * and the processing of returning customers which are coming from redirect payment
+     * processes via the success-, error- and backurl parameters.
+     *
 	 * @param int $order_id
 	 * @param string $transaction_class
-	 * @return array
+	 * @return array|void
 	 * @throws \WC_Data_Exception
 	 */
 	public function process_redirect( $order_id, $transaction_class ) {
 		$order = new \WC_Order( $order_id );
 
-		$is_success = false;
-		$make_redirect = false;
 		if ( $this->is_redirect( 'success' ) ) {
-			$make_redirect = true;
-			$is_success = $order->get_meta( '_appointed' ) > 0;
-			if ( ! $is_success ) {
-			    error_log("No appointed flag set for order {$order->get_id()} before the customer received the checkout success page.");
-				// wc_add_notice( __( 'Payment error: ',
-				// 		'payone-woocommerce-3' ) . __( 'Did not receive "appointed" callback',
-				// 		'payone-woocommerce-3' ),
-				// 	'error' );
-			}
-		} elseif ( $this->is_redirect( 'error' ) ) {
-			$make_redirect = true;
-			$is_success = false;
-			wc_add_notice( __( 'Payment error: ', 'payone-woocommerce-3' ) . __( 'Payment provider returned error',
-					'payone-woocommerce-3' ), 'error' );
-		} elseif ( $this->is_redirect( 'back' ) ) {
-			$make_redirect = true;
-			$is_success = false;
-			wc_add_notice( __( 'Payment error: ', 'payone-woocommerce-3' ) . __( 'Payment was canceled by user',
-					'payone-woocommerce-3' ), 'error' );
-		} else {
+            // We are back in the shop via the provided successurl
+
+		    // Log missing appointed flag for this order.
+            // Maybe the shop was too slow returning TSOK for the APPOINTED
+            // TX status notification.
+		    if ( $order->get_meta( '_appointed' ) > 0 ) {
+                error_log("No appointed flag set for order {$order->get_id()} before the customer received the checkout success page.");
+            }
+
+            $this->handle_successfull_payment( $order );
+            $target_url = $this->get_return_url( $order );
+
+            wp_redirect( $target_url );
+            exit;
+        } else if ( $this->is_redirect( 'error' ) ) {
+            // We are back in the shop via the provided errorurl
+
+            wc_add_notice( __( 'Payment error: ', 'payone-woocommerce-3' ) . __( 'Payment provider returned error',
+                    'payone-woocommerce-3' ), 'error' );
+
+            wp_redirect( wc_get_checkout_url() );
+            exit;
+        } else if ( $this->is_redirect( 'back' ) ) {
+            // We are back in the shop via the provided backurl
+
+            wc_add_notice( __( 'Payment error: ', 'payone-woocommerce-3' ) . __( 'Payment was canceled by user',
+                    'payone-woocommerce-3' ), 'error' );
+
+            wp_redirect( wc_get_checkout_url() );
+            exit;
+        } else {
+		    // We are initiating the payment and may redirect the customer depending on the
+            // PAYONE API response. Especially for credit cards sometimes redirects occur
+            // sometimes not.
+
 			/** @var \Payone\Transaction\Base $transaction */
 			$transaction = new $transaction_class( $this );
 
@@ -48,6 +66,7 @@ abstract class RedirectGatewayBase extends GatewayBase {
 			$order->save_meta_data();
 			$order->save();
 
+			// Perform the redirect if we need to
 			if ( $response->is_redirect() ) {
 				return [
 					'result'   => 'success',
@@ -55,37 +74,22 @@ abstract class RedirectGatewayBase extends GatewayBase {
 				];
 			}
 
+			// At this point a redirect was not required,
+            // we are processing the payment as regular.
 			if ( $response->has_error() ) {
+			    // According to the WooCommerce docs we just return nothing to indicate a payment error
 				wc_add_notice( __( 'Payment error: ', 'payone-woocommerce-3' ) . $response->get_error_message(), 'error' );
 			} else {
-				$is_success = true;
+			    // No error, complete the payment
+                $this->handle_successfull_payment( $order );
+                $target_url = $this->get_return_url( $order );
+
+                return [
+                    'result'   => 'success',
+                    'redirect' => $target_url,
+                ];
 			}
 		}
-
-		if ( $is_success ) {
-			$this->handle_successfull_payment( $order );
-			$target_url = $this->get_return_url( $order );
-
-			if ( $make_redirect ) {
-				wp_redirect( $target_url );
-				exit;
-			}
-
-			return array(
-				'result'   => 'success',
-				'redirect' => $target_url,
-			);
-		}
-
-		if ( $make_redirect ) {
-			wp_redirect( wc_get_checkout_url() );
-			exit;
-		}
-
-		return [
-			'result'   => 'error',
-			'redirect' => wc_get_checkout_url(),
-		];
 	}
 
 	private function handle_successfull_payment( \WC_Order $order ) {
