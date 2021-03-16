4d43bb23e25d3dfccd9e2d7fb674377d88dd8f16
Zoran Vidakovic <zoran.vidakovic@pooliestudios.com>
2021-03-16 11:56:49 +0100

    - Reverting entry script - Moved class properties to methods for backwards compatibility

--- payone-woocommerce-3.php
+++ payone-woocommerce-3.php
@@ -19,8 +19,8 @@ define( 'PAYONE_VIEW_PATH', PAYONE_PLUGIN_PATH . '/views' );
 
 require_once 'src/autoload.php';
 
-add_action( 'woocommerce_loaded', function () {
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	$payonePlugin = new \Payone\Plugin();
 	add_action( 'init', [ $payonePlugin, 'add_callback_url' ] );
 	add_action( 'plugins_loaded', [ $payonePlugin, 'init' ] );
-} );
+}
