d29f32b6becaf68c76eef7e1bbaf98e1a4e68490
Jan Teuber <jan.teuber@payone.com>
2022-03-17 15:53:25 +0100

    version bump to 200

--- payone-woocommerce-3.php
+++ payone-woocommerce-3.php
@@ -2,18 +2,18 @@
 
 /**
  * Plugin Name: PAYONE Payment for WooCommerce
- * Version: 1.7.1
+ * Version: 2.0.0
  * Plugin URI: https://www.payone.com/
  * Description: Integration of PAYONE payment into your WooCommerce store.
  * Author: PAYONE GmbH
  * Author URI: https://www.payone.com/
  * Requires at least: 5.0
- * Tested up to: 5.5
+ * Tested up to: 5.9.2
  */
 
 defined( 'ABSPATH' ) or die( 'Direct access not allowed' );
 
-define( 'PAYONE_PLUGIN_VERSION', '1.7.1' );
+define( 'PAYONE_PLUGIN_VERSION', '2.0.0' );
 define( 'PAYONE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PAYONE_PLUGIN_PATH', __DIR__ );
 define( 'PAYONE_VIEW_PATH', PAYONE_PLUGIN_PATH . '/views' );
