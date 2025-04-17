6f2a8f95c32ec91dec4863da336adb19398a9b46
Dirk Olbertz <dirk.olbertz+pplan@publicplan.de>
2025-04-17 08:21:23 +0200

    #1 Block-Zahlarten alle wieder aktiviert

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7d66adfab2d89f43fd03');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '875f86c1c522f955ac4b');
