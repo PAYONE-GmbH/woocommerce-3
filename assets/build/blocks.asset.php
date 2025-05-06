34f70b5c9d962a758baa1a4b65305f7868a478cb
Lucas Carl <lucas.carl@emperon.studio>
2025-05-06 13:44:24 +0200

    #3 remove old payment methods

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '875f86c1c522f955ac4b');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '685932f38bf5e02ed072');
--- client/blocks/trustly/index.jsx
+++
@@ -1,9 +0,0 @@
-import {__} from '@wordpress/i18n';
-import {PAYONE_ASSETS_URL} from '../../constants';
-import getPaymentMethodConfig from '../../services/getPaymentMethodConfig';
-
-export default getPaymentMethodConfig(
-    'payone_trustly',
-    __('PAYONE Trustly', 'payone-woocommerce-3'),
-    `${PAYONE_ASSETS_URL}/icon-trustly.png`,
-);
