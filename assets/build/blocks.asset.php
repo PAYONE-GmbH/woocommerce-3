946f497733f575f113dd739b74277050b833d6b3
Lucas Carl <lucas.carl@emperon.studio>
2025-05-06 16:00:39 +0200

    #4 make creditcard card holder field customizable

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '685932f38bf5e02ed072');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c9341435ea16027dc25c');
