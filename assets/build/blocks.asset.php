0a49f93c51de2a30f6c495642bfe370c7258397f
René Woizenko <rene.woizenko@emperon.studio>
2025-10-06 15:20:15 +0200

    feat(#8): generate translation JSON for WooCommerce Blocks
    - Add script for generating language-specific JSON files.
    - Include `.po` files for `de_DE`, `de_DE_formal`, `de_CH`, and `de_CH_informal`.
    - Update asset dependencies for blocks.

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => 'da7376f9a1625ca971d4');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => 'c67efb9ddb7f81244868');
