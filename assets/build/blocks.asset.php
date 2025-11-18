b4307edc020027016390ffe7a2682e0374be0b0f
Dirk Olbertz <dirk.olbertz@emperon.studio>
2025-11-18 14:14:34 +0100

    #14 after build:webpack

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => 'b599d6b7ec1ffc4c1f8f');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => '986e57c2bf4613b0a105');
