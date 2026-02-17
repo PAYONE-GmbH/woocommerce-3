aaa94785363e9d1fe62477aea17570c1f9a0a985
René Woizenko <rene.woizenko@emperon.studio>
2026-02-17 11:49:59 +0100

    Rebuild frontend block assets

--- assets/build/blocks.asset.php
+++ assets/build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => 'fb23882618b9a3f8e83c');
+<?php return array('dependencies' => array('wc-blocks-checkout', 'wc-blocks-registry', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal'), 'version' => 'ff9fcaf66f0366d192aa');
